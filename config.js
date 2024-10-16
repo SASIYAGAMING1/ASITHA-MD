61b651d6f5f44e0edf517fb4e57fd907636e3aa3
SASIYAGAMING1 <147159667+SASIYAGAMING1@users.noreply.github.com>
2024-10-16 19:01:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=WvYA2JJS#ZpHpyCJkdllN9QaqePJxGRa8fvbJV0Y3Ax6j4maG7ns",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=CvhmgLpb#yN_jmHCUESOFRAoeGfTFR49Qbqy3wreRI0L2AU1mWIg",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
